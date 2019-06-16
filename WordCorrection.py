11f02bc268e2f586304e4a477c04861667e22a78
prachijustin <prachi.sharma01sep@gmail.com>
2019-06-16 11:21:58 +0530

    Second commit

--- WordCorrection.py
+++ WordCorrection.py
@@ -1,56 +1,88 @@
-#program to predict the correct word for wrongly typed word
-
 import re
-import spellchecker
+from collections import Counter
 
-#creating a spellchecker object---spell
-spell = spellchecker.SpellChecker(language=None) # turning off loading a built-in language dictionary
+f = open('big.txt').read()
+array = re.findall(r'\w+', f.lower())   
+ 
+final_words = Counter(array)
+#print(len(final_words))
+n = sum(final_words.values())
 
 
-try:
-    #working with big.txt corpus
-    with open('big.txt', 'r') as file:
-        f = file.read()
-        words = []
-        for word in f.split():
-            words.append(word)   #creating a list of words present in corpus
+#making combinations of entered text by 1-distance
+#correcting the word by one letter
+def combinationsOne(txt):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    insert_words = []
+    for i in range(len(txt)+1):
+        for l in letters:
+            insert_words.append(txt[:i]+ l + txt[i:])
     
-    #filtering out all words except special chars & numbers using Regular Expression
-    array = re.sub('[^a-zA-Z]', ' ', f)
-   
-   #writing the filtered words into myfile.txt
-    with open('myfile.txt', 'w') as myfile:
-        myfile.write(array)
+    deleted_words = []
+    t = txt
+    for i in range(len(txt)):
+        t = txt
+        deleted_words.append(txt[:i] +  txt[i+1:])
 
-    with open('myfile.txt','r') as  mfile:
-        ff = mfile.read()
+    replace_words = []
+    for i in range(len(txt)):
+        for l in letters:
+            replace_words.append(txt[:i] + l +txt[i+1:])
+    
+    transpose_words = []
+    for i in range(0,len(txt)-1):
+        t=list(txt)
+        t[i], t[i+1] = t[i+1], t[i]
+        transpose_words.append(''.join(t))
 
-    #appending final words into a list
-    final_words=[]
-    for word in ff.split():
-        final_words.append(word)
-
-
-except FileNotFoundError:
-    print('File not found')
-
-except:
-    print('Cannot open file')
+    comb1_words = set(insert_words + deleted_words + replace_words + transpose_words)
+    return comb1_words
 
 
+#making combinations of entered text by 2-distance
+#correcting the word by two letters
+def combinationsTwo(txt):
+    comb2_words = []
+    for w1 in combinationsOne(txt):
+        for w2 in combinationsOne(w1):
+            comb2_words.append(w2)
+    return comb2_words
 
-#loading our own corpus--- it loads as words as in list 
-#in spell object
-spell.word_frequency.load_text_file('./myfile.txt')
 
-txt = input('Enter a word: ')
 
-#correccting the spelling entered by used using the same spell object
-x = spell.correction(txt)
-print('Correct spelling is: ',x)
+#finding out possible(known) words from the original list
+def possibleWords(txt):
+    possible_list = []
+    for w in txt:
+        if w in final_words:
+            possible_list.append(w)
+    return set(possible_list)
 
-#finding probability of corrected word
-print('Probability of correct word: ',spell.word_probability(x,  total_words=len(final_words)))
 
-#other possible words 
-print('Other possible words: ',spell.candidates(txt))
+#finding out candidate words
+def candidateWords(txt):
+    candidate1 = txt
+    candidate2 = possibleWords([txt])
+    candidate3 = possibleWords(combinationsOne(txt))
+    candidate4 = possibleWords(combinationsTwo(txt))
+    return (candidate2 or candidate3 or candidate4 or candidate1)
+
+
+#calculating number of times entered word present in original list
+def frequencyWords(txt):   
+    frequency = final_words[txt] / n
+    return frequency
+
+
+#correcting the spelling of words
+#it finds the candidate word that appeared max no. of times
+def correctWord(txt):
+    #frequencyWords = final_words[txt] / n
+    correct = max(candidateWords(txt), key = frequencyWords)
+    return correct
+
+
+txt = input('Enter input: ')
+print('Correct word: ', correctWord(txt))
+print('Other possible words: ', candidateWords(txt))
+print('Frequency of correct word: ' ,frequencyWords(correctWord(txt)))
